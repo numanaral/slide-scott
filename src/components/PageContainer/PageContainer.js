5921919e17a872b8a1635070edfb9cee030a5623
numanaral <ahmet.aral@ucalgary.ca>
2020-11-12 12:56:03 -0700

    update <PageContainer />

--- src/components/PageContainer/PageContainer.js
+++ src/components/PageContainer/PageContainer.js
@@ -2,24 +2,21 @@ import { Paper } from '@material-ui/core';
 import styled from 'styled-components';
 
 const PageContainer = styled(Paper)`
-	&.MuiPaper-root {
-		background-color: #424242;
-		color: white;
-	}
+	${({ theme }) => `
 	width: 100%;
 	height: 100%;
-	justify-content: center;
+	padding: ${theme.spacing(2)}px;
 	text-align: center;
 	display: flex;
 	flex-direction: column;
 	justify-content: flex-start;
 	overflow-y: auto;
 	overflow-x: hidden;
-	padding: 16px;
-	@media only screen and (min-width: 768px) {
+	${theme.breakpoints.up('md')} {
 		justify-content: center;
-		padding: 80px;
+		padding: theme.spacing(10);
 	}
+`}
 `;
 
 export default PageContainer;
