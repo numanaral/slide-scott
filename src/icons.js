5b73470efb277c4f8e245c60b2cc68f88993b643
numanaral <ahmet.aral@ucalgary.ca>
2020-11-12 12:57:10 -0700

    new icons

--- src/icons.js
+++ src/icons.js
@@ -1,1 +1,5 @@
-export { SentimentVeryDissatisfied as SadIcon } from '@material-ui/icons';
+export {
+	WbSunny as LightThemeIcon,
+	Brightness2 as DarkThemeIcon,
+	SentimentVeryDissatisfied as SadIcon,
+} from '@material-ui/icons';
