6199f7404be15c7ed608be32f76d2016c6089753
numanaral <ahmet.aral@ucalgary.ca>
2020-11-13 04:06:23 -0700

    full sized grid to prevent rewriting this countless times

---
+++ src/components/FullSizeGrid/FullSizeGrid.js
@@ -0,0 +1,9 @@
+import { Grid } from '@material-ui/core';
+import styled from 'styled-components';
+
+const FullSizeGrid = styled(Grid)`
+	width: 100%;
+	height: 100%;
+`;
+
+export default FullSizeGrid;
