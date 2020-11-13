9cc7b3e91d6647619d9b4e522d72a27e8122d3e4
numanaral <ahmet.aral@ucalgary.ca>
2020-11-13 04:53:50 -0700

    allow spacing wrapper to have dynamic spacing

--- src/components/ContainerSpacingWrapper/ContainerSpacingWrapper.js
+++ src/components/ContainerSpacingWrapper/ContainerSpacingWrapper.js
@@ -5,8 +5,8 @@ import FullSizeGrid from 'components/FullSizeGrid';
 import { propTypes, defaultProps } from './types';
 
 const Wrapper = styled(FullSizeGrid)`
-	${({ theme, $topBottom, $rightLeft }) => {
-		const spacing = `${theme.spacing(1)}px`;
+	${({ theme, $topBottom, $rightLeft, $spacing }) => {
+		const spacing = `${theme.spacing($spacing || 1)}px`;
 		const topBottom = ($topBottom && spacing) || '0';
 		const rightLeft = ($rightLeft && spacing) || '0';
 		return `
@@ -15,8 +15,18 @@ const Wrapper = styled(FullSizeGrid)`
 	}}
 `;
 
-const ContainerSpacingWrapper = ({ topBottom, rightLeft, ...rest }) => (
-	<Wrapper $topBottom={topBottom} $rightLeft={rightLeft} {...rest} />
+const ContainerSpacingWrapper = ({
+	topBottom,
+	rightLeft,
+	spacing,
+	...rest
+}) => (
+	<Wrapper
+		$topBottom={topBottom}
+		$rightLeft={rightLeft}
+		$spacing={spacing}
+		{...rest}
+	/>
 );
 
 ContainerSpacingWrapper.propTypes = propTypes;
