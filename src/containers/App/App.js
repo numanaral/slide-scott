4c3f480f2817e296858920bc72df23f99481860f
numanaral <ahmet.aral@ucalgary.ca>
2020-12-01 22:30:56 -0700

    Wrap app with ReactReduxFirebaseProvider

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider as StoreProvider } from 'react-redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
 
 import ThemeProvider from 'providers/ThemeProvider';
 import NavigationBar from 'components/NavigationBar';
 import Routes from 'routes';
 import configureStore from 'store/redux';
+import { getReactReduxFirebaseProps } from 'store/firebase';
+import NotificationProvider from 'providers/NotificationProvider';
 import Background from './Background';
 
 const initialState = {};
@@ -15,16 +18,21 @@ if (process.env.NODE_ENV !== 'production') window.__redux_store = store;
 
 const App = () => (
 	<StoreProvider store={store}>
-		<Router>
-			<ThemeProvider>
-				{themeProps => (
-					<Background>
-						<NavigationBar {...themeProps} />
-						<Routes />
-					</Background>
-				)}
-			</ThemeProvider>
-		</Router>
+		<ReactReduxFirebaseProvider {...getReactReduxFirebaseProps(store)}>
+			<Router>
+				<ThemeProvider>
+					{themeProps => (
+						<>
+							<NavigationBar {...themeProps} />
+							<NotificationProvider />
+							<Background>
+								<Routes />
+							</Background>
+						</>
+					)}
+				</ThemeProvider>
+			</Router>
+		</ReactReduxFirebaseProvider>
 	</StoreProvider>
 );
 
