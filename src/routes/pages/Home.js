b2c7d516c2bef3c459ca1d36028958e4e748e957
numanaral <ahmet.aral@ucalgary.ca>
2020-12-01 22:35:54 -0700

    Update Home page

--- src/routes/pages/Home.js
+++ src/routes/pages/Home.js
@@ -1,6 +1,59 @@
 import React from 'react';
-import LazySlideBuilder from 'containers/SlideBuilder/Lazy';
+import styled from 'styled-components';
+import { Typography } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
-const Home = () => <LazySlideBuilder>Home Page</LazySlideBuilder>;
+import useNotificationProvider from 'store/redux/hooks/useNotificationProvider';
+import ContainerWithCenteredItems from 'components/ContainerWithCenteredItems';
+import TooltipButton from 'components/TooltipButton';
+import { BASE_PATH } from 'routes/constants';
+import Spacer from 'components/Spacer';
+
+const StyledTypography = styled(Typography)`
+	font-size: 150px;
+`;
+
+const GetStartedButton = styled(TooltipButton)`
+	height: 90px;
+	width: 350px;
+`;
+
+const TutorialButton = styled(TooltipButton)`
+	height: 50px;
+	width: 200px;
+`;
+
+const Home = () => {
+	const { notifyInfo } = useNotificationProvider();
+	const { push } = useHistory();
+	return (
+		<>
+			<Spacer direction="bottom" spacing="5" />
+			<ContainerWithCenteredItems>
+				<StyledTypography variant="h1" component="h1" color="secondary">
+					SlideScott
+				</StyledTypography>
+			</ContainerWithCenteredItems>
+			<Spacer direction="bottom" spacing="3" />
+			<ContainerWithCenteredItems>
+				<GetStartedButton
+					tooltip="Create an account and start creating slides."
+					text="Get Started."
+					onClick={() => push(`${BASE_PATH}/slide-builder`)}
+					bg="secondary"
+				/>
+			</ContainerWithCenteredItems>
+			<Spacer direction="bottom" spacing="3" />
+			<ContainerWithCenteredItems>
+				<TutorialButton
+					tooltip="Watch a tutorial on how to use SlideScott."
+					text="Tutorial"
+					onClick={() => notifyInfo('Coming soon')}
+					bg="primary"
+				/>
+			</ContainerWithCenteredItems>
+		</>
+	);
+};
 
 export default Home;
