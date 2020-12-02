17bdf5378365380c10bad0103d67018a958e3366
numanaral <ahmet.aral@ucalgary.ca>
2020-12-02 04:20:19 -0700

    Update Preference page

--- src/routes/pages/Preferences.js
+++ src/routes/pages/Preferences.js
@@ -1,7 +1,89 @@
+import ContainerWithCenteredItems from 'components/ContainerWithCenteredItems';
 import React from 'react';
+import { Switch as MuiSwitch } from '@material-ui/core';
 
-import Container from 'components/PageContainer';
+import { ThemeIcon, AnimationIcon, PaletteIcon } from 'icons';
+import LazyList from 'components/List/Lazy';
+import { itemTypes, SPACER } from 'components/List/constants';
+import ThemeToggle from 'containers/ThemeToggle';
+import TooltipButton from 'components/TooltipButton';
+import PaperContainerWithSpacing from 'components/PaperContainerWithSpacing';
 
-const Preferences = () => <Container>Preferences Page</Container>;
+const noop = () => null;
+
+const ComingSoon = () => (
+	<TooltipButton
+		tooltip="Pick a color"
+		text="Pick a color"
+		variant="contained"
+		color="primary"
+		icon={PaletteIcon}
+		onClick={noop}
+		disabled
+	/>
+);
+const Switch = () => <MuiSwitch disabled />;
+
+const PREFERENCE_LIST = [
+	{
+		type: itemTypes.SECTION_TITLE_WITH_DIVIDER,
+		primaryText: 'VIEW SETTINGS',
+	},
+	{
+		primaryText: 'Theme',
+		icon: ThemeIcon,
+		// TODO: If I decide to add redux, won't need this complication here
+		// but it's not needed right now
+		secondaryAction: <ThemeToggle />,
+	},
+	{
+		primaryText: 'Primary Color',
+		icon: PaletteIcon,
+		secondaryAction: ComingSoon,
+	},
+	{
+		primaryText: 'Secondary Color',
+		icon: PaletteIcon,
+		secondaryAction: ComingSoon,
+	},
+	{
+		type: itemTypes.SECTION_TITLE,
+		primaryText: '... more color settings',
+	},
+	SPACER,
+	{
+		type: itemTypes.SECTION_TITLE_WITH_DIVIDER,
+		primaryText: 'ANIMATIONS',
+		secondaryText: 'Disable/Modify animations',
+	},
+	{
+		primaryText: 'Animations',
+		secondaryText: 'Turns on/off all the animations',
+		icon: AnimationIcon,
+		secondaryAction: Switch,
+		disabled: true,
+	},
+	SPACER,
+	{
+		type: itemTypes.SECTION_TITLE_WITH_DIVIDER,
+		primaryText: 'SOME OTHER SETTINGS',
+		secondaryText: 'Moar settings',
+	},
+	{
+		primaryText: 'Some setting',
+		secondaryText: 'Does something',
+		icon: AnimationIcon,
+		secondaryAction: Switch,
+		disabled: true,
+	},
+];
+
+const Preferences = () => (
+	<ContainerWithCenteredItems vertical horizontal>
+		<PaperContainerWithSpacing padding="50px !important">
+			<LazyList list={PREFERENCE_LIST} />
+		</PaperContainerWithSpacing>
+	</ContainerWithCenteredItems>
+);
 
 export default Preferences;
