77a87d91ea0bbdd0dc49f205a3f6991143cf272d
numanaral <ahmet.aral@ucalgary.ca>
2020-12-01 22:29:05 -0700

    NotificationProvider + reorganize folder structure

---
+++ src/providers/NotificationProvider.js
@@ -0,0 +1,18 @@
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Snackbar from 'components/Snackbar';
+import makeSelectSnackbarProps from 'store/redux/reducers/snackbar/selector';
+import { hideNotification } from 'store/redux/reducers/snackbar/actions';
+
+const NotificationProvider = () => {
+	const snackbarProps = useSelector(makeSelectSnackbarProps());
+	const dispatch = useDispatch();
+	const hideSnackbar = useCallback(() => dispatch(hideNotification()), [
+		// dispatch,
+	]);
+
+	return <Snackbar {...snackbarProps} onClose={hideSnackbar} />;
+};
+
+export default NotificationProvider;
