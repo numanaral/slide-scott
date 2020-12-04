fbdaebf7febf9a3e034e6a358f3bff6b3d1f15b1
numanaral <ahmet.aral@ucalgary.ca>
2020-12-04 13:13:18 -0700

    Update toolbox with the new config & components

--- src/containers/SlideBuilder/Toolbox/Toolbox.js
+++ src/containers/SlideBuilder/Toolbox/Toolbox.js
@@ -1,12 +1,21 @@
-import React, { createElement, useEffect, useRef, useState } from 'react';
-import { Grid } from '@material-ui/core';
+import React, {
+	createElement,
+	Fragment,
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
+import { Grid, Typography } from '@material-ui/core';
 
 import FullSizeGrid from 'components/FullSizeGrid';
-import * as icons from './icons';
+// import * as icons from './icons';
+import ContainerWithCenteredItems from 'components/ContainerWithCenteredItems';
+import Spacer from 'components/Spacer';
 import { propTypes, defaultProps } from './types';
 import Tool, { MARGIN } from './Tool';
 import ContainerTitle from '../shared/ContainerTitle';
 import ContainerBody from '../shared/ContainerBody';
+import TOOLS from './config';
 
 const Toolbox = ({ onDragStart }) => {
 	const toolboxRef = useRef();
@@ -29,41 +38,63 @@ const Toolbox = ({ onDragStart }) => {
 		};
 	}, []);
 
+	const toolList = Object.keys(TOOLS).reduce(
+		(acc, key) => {
+			const tool = TOOLS[key];
+			acc[(tool.interactive && 'Interactive') || 'Static'].push(tool);
+			return acc;
+		},
+		{
+			Interactive: [],
+			Static: [],
+		}
+	);
+
 	return (
 		<FullSizeGrid innerRef={toolboxRef}>
 			<ContainerTitle> Tools </ContainerTitle>
 			<ContainerBody container alignContent="flex-start">
-				{Object.keys(icons).map(icon => (
-					<Tool
-						container
-						key={icon}
-						tool-size={toolSize}
-						draggable="true"
-						onDragStart={onDragStart}
-						draggable-classifier={`draggable-${icon.toLowerCase()}`}
-					>
-						<Grid
-							className="icon-text"
-							container
-							item
-							xs={12}
-							justify="center"
-							alignItems="flex-end"
-						>
-							{icon}
-						</Grid>
-						<Grid
-							container
-							item
-							xs={12}
-							justify="center"
-							alignItems="flex-start"
-						>
-							{createElement(icons[icon])}
-						</Grid>
-					</Tool>
+				{Object.keys(toolList).map(key => (
+					<Fragment key={key}>
+						<Spacer direction="top" spacing="2" />
+						<ContainerWithCenteredItems horizontal>
+							<Typography>{key}</Typography>
+						</ContainerWithCenteredItems>
+						{toolList[key].map(
+							({ settings: { icon, name, type } }) => (
+								<Tool
+									container
+									key={name}
+									tool-size={toolSize}
+									draggable="true"
+									onDragStart={onDragStart}
+									draggable-type={type}
+								>
+									<Grid
+										className="icon-text"
+										container
+										item
+										xs={12}
+										justify="center"
+										alignItems="flex-end"
+										style={{ textAlign: 'center' }}
+									>
+										{name}
+									</Grid>
+									<Grid
+										container
+										item
+										xs={12}
+										justify="center"
+										alignItems="flex-start"
+									>
+										{createElement(icon)}
+									</Grid>
+								</Tool>
+							)
+						)}
+					</Fragment>
 				))}
-				more tools here...
 			</ContainerBody>
 		</FullSizeGrid>
 	);
--- src/containers/SlideBuilder/Toolbox/icons.js
+++
@@ -1,8 +0,0 @@
-export {
-	Image,
-	Movie as Video,
-	Audiotrack as Audio,
-	Assignment as Text,
-	Link,
-	ChangeHistory as Shape,
-} from '@material-ui/icons';
