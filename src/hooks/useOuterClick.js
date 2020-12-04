aeb60d30718a0af3d75ac60c5c88f929f01227be
numanaral <ahmet.aral@ucalgary.ca>
2020-12-04 13:11:21 -0700

    hook to catch clicks outside the ref component

---
+++ src/hooks/useOuterClick.js
@@ -0,0 +1,30 @@
+const { useRef, useEffect } = require('react');
+
+/** @see https://stackoverflow.com/a/54292872/13161405 */
+const useOuterClick = cb => {
+	const callbackRef = useRef(); // initialize mutable callback ref
+	const innerRef = useRef(); // returned to client, who sets the "border" element
+
+	// update callback on each render, so second useEffect has most recent callback
+	useEffect(() => {
+		callbackRef.current = cb;
+	});
+	useEffect(() => {
+		const handleClick = e => {
+			if (
+				innerRef.current &&
+				callbackRef.current &&
+				!innerRef.current.contains(e.target) &&
+				innerRef.current.classList.contains('isFocused')
+			) {
+				callbackRef.current(e);
+			}
+		};
+		document.addEventListener('click', handleClick);
+		return () => document.removeEventListener('click', handleClick);
+	}, []); // no dependencies -> stable click listener
+
+	return innerRef; // convenience for client (doesn't need to init ref himself)
+};
+
+export default useOuterClick;
