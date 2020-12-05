73228b05d9f4eab9035d034993625ee5b65b0b6f
numanaral <ahmet.aral@ucalgary.ca>
2020-12-05 13:20:48 -0700

    firebase utils to convert data in-between requests

--- src/store/firebase/utils.js
+++ src/store/firebase/utils.js
@@ -1,6 +1,51 @@
+import firebase from 'firebase/app';
+
 const sortByFirebaseDate = (sort = 'desc') => (a, b) =>
-	((sort === 'desc' && 1) || -1) * a.updatedOn.seconds - b.updatedOn.seconds;
+	((sort === 'desc' && -1) || 1) *
+	(a.updatedOn.seconds - b.updatedOn.seconds);
 
-const getDateFromFirebaseObject = obj => new Date(obj.seconds * 1000);
+// Can be resolved with const date = updatedOn.toDate();
+// const getDateFromFirebaseObject = obj => new Date(obj.seconds * 1000);
 
-export { sortByFirebaseDate, getDateFromFirebaseObject };
+/**
+ * Stringifies objects before sending them to firebase
+ * @param {Object} props Properties to be passed into the firebase request
+ */
+const toFirestore = props =>
+	Object.keys(props).reduce((acc, key) => {
+		const value = props[key];
+		// eslint-disable-next-line no-param-reassign
+		acc[key] =
+			(typeof value === 'object' &&
+				!(value instanceof Date) &&
+				JSON.stringify(value)) ||
+			value;
+		return acc;
+	}, {});
+
+/**
+ * Parses stringified objects before passing them to the client
+ * @param {Object} props Properties returned from the firebase request
+ * @param {Array<String>} keys List of keys that need their value string parsed as JSON object
+ */
+const fromFirestore = (props, keys) =>
+	Object.keys(props).reduce((acc, key) => {
+		const value = props[key];
+		// eslint-disable-next-line no-param-reassign
+		acc[key] =
+			(keys.includes(key) && JSON.parse(value)) ||
+			(value instanceof firebase.default.firestore.Timestamp &&
+				value.toDate()) ||
+			value;
+		return acc;
+	}, {});
+
+const mapFromFireStore = (keys = []) => props => fromFirestore(props, keys);
+
+export {
+	sortByFirebaseDate,
+	// getDateFromFirebaseObject,
+	toFirestore,
+	fromFirestore,
+	mapFromFireStore,
+};
