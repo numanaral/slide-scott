aab67150c5bea154ac9f6f479a27729af7e8a161
Numan <ahmet.aral@ucalgary.ca>
2021-04-29 12:12:58 -0600

    show not-mobile-friendly popup once, and at < xs

--- src/hooks/useIsNotMobileFriendlyHook.js
+++ src/hooks/useIsNotMobileFriendlyHook.js
@@ -1,11 +1,18 @@
 import { useMediaQuery } from '@material-ui/core';
 import useNotificationProvider from 'store/redux/hooks/useNotificationProvider';
+import useLocalStorage from './useLocalStorage';
 
+const NOT_MOBILE_FRIENDLY_LOCAL_STORAGE_KEY = 'not-mobile-friendly';
 const useIsNotMobileFriendly = () => {
 	const { notifyWarning } = useNotificationProvider();
-	const smAndDown = useMediaQuery(theme => theme.breakpoints.down('sm'));
+	const smAndDown = useMediaQuery(theme => theme.breakpoints.down('xs'));
+	const [seenPopUp, setSeenPopup] = useLocalStorage(
+		NOT_MOBILE_FRIENDLY_LOCAL_STORAGE_KEY,
+		false
+	);
 
-	if (smAndDown) {
+	if (!seenPopUp && smAndDown) {
+		setSeenPopup(true);
 		notifyWarning(
 			`This website was not intended to be mobile friendly for its initial purpose. Please use on a larger screen until this changes in the future!`
 		);
